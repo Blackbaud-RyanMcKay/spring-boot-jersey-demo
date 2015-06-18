d83b401b8ba97febe1b982c1014d815c981aa06c
Ryan McKay <ryan.mckay@blackbaud.com>
2015-06-18 16:38:31 -0500

    Replace manual health check with actuator

--- src/main/java/pl/codeleak/demo/web/JerseyConfig.java
+++ src/main/java/pl/codeleak/demo/web/JerseyConfig.java
@@ -3,8 +3,6 @@ package pl.codeleak.demo.web;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import pl.codeleak.demo.web.CustomerController;
-import pl.codeleak.demo.web.HealthController;
 
 import javax.ws.rs.ApplicationPath;
 
@@ -14,7 +12,6 @@ import javax.ws.rs.ApplicationPath;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        register(HealthController.class);
         register(CustomerController.class);
     }
 }
